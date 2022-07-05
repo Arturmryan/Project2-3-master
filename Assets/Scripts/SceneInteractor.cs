dc0cf7eec8d6f4bbc718694854a2c029c95ef120
Arturmryan <arturmryan@gmail.com>
2022-07-05 01:56:10 -0400

    added color script

--- Assets/Scripts/SceneInteractor.cs
+++ Assets/Scripts/SceneInteractor.cs
@@ -11,18 +11,9 @@ namespace com
         {
             public class SceneInteractor : MonoBehaviour
             {
-                // Start is called before the first frame update
-                void Start()
-                {
-
-                }
-
-                // Update is called once per frame
-                void Update()
-                {
-
-                }
-
+               
+                public GameObject[] pointLights;
+                LightingIntensityValue myIntensValue;
                 void SetDoorOpen(GameObject door)
                 {   
                     /**
@@ -34,19 +25,38 @@ namespace com
 
                 void SetLightingIntensity(GameObject go)
                 {
-                   /**
-                        The argument "go" is a GameObject datatype that will contain a reference to either of the intensity cubes on the floor (the ones from white to black).
-                        These GameObject cubes have a script attached to them called as "LightingIntensityValue". 
-                        You need to access this script as a component and read the public float variable "intensity".
-                        The value in the intensity will be set as the intensity to all the lights in the scene marked with the tag "DynamicLights".
-                        To access all GameObjects in the scene with a tag you need to use GameObject.FindGameObjectsWithTag("DynamicLights")
-                        Once you have all the GameObjects then you need to access their component "<Light>" to which you will set the intensity value.
-                        To know how to set the value of intensity visit: https://docs.unity3d.com/ScriptReference/Light-intensity.html
-                   **/
+                    pointLights = GameObject.FindGameObjectsWithTag("DynamicLights");
+
+                    foreach (GameObject pointLight in pointLights)
+                    {
+                        myIntensValue.GetComponent<LightingIntensityValue>().intensity = 
+                        pointLight.GetComponent<Light>().intensity = ;
+                        
+
+                    }
+                    /**
+                         The argument "go" is a GameObject datatype that will contain a reference to either of the intensity cubes on the floor (the ones from white to black).
+                         These GameObject cubes have a script attached to them called as "LightingIntensityValue". 
+                         You need to access this script as a component and read the public float variable "intensity".
+                         The value in the intensity will be set as the intensity to all the lights in the scene marked with the tag "DynamicLights".
+                         To access all GameObjects in the scene with a tag you need to use GameObject.FindGameObjectsWithTag("DynamicLights")
+                         Once you have all the GameObjects then you need to access their component "<Light>" to which you will set the intensity value.
+                         To know how to set the value of intensity visit: https://docs.unity3d.com/ScriptReference/Light-intensity.html
+                    **/
                 }
+                
 
                 void SetLightingColor(Color c)
                 {
+                    pointLights =  GameObject.FindGameObjectsWithTag("DynamicLights");
+
+                    foreach (GameObject pointLight in pointLights)
+                    {
+                        pointLight.GetComponent<Light>().color = c;
+                        //c = new Color();
+                        
+                    }
+                    
                     /**
                         The argument "c" is a Color datatype that will contain either of the values as Color.red, Color.green, or Color.blue.
                         To access all GameObjects in the scene with a tag you need to use GameObject.FindGameObjectsWithTag("DynamicLights")
@@ -59,6 +69,31 @@ namespace com
                 {
                     Debug.Log("HIT A COLLIDER " + c.gameObject.name);
                     string gameObjectThatWasHit = c.gameObject.name;
+                    
+                    if (c.gameObject.name == "RedCube")
+                    {
+                        SetLightingColor(Color.red);
+
+                        //Destroy(c.gameObject);
+                    }
+                    else if (c.gameObject.name == "BlueCube")
+                    {
+                        SetLightingColor(Color.blue);
+
+                        //Destroy(c.gameObject);
+                    }
+                    else if (c.gameObject.name == "GreenCube")
+                    {
+                        SetLightingColor(Color.green);
+
+                        //Destroy(c.gameObject);
+                    }
+                    else if (c.gameObject.name == "RugCarpet")
+                    {
+                        SetLightingColor(Color.white);
+
+                        //Destroy(c.gameObject);
+                    }
                     /**
                         This function is triggerred whenever your character collides with any of the colored cubes, intensity cubes, or the door, or the RugCarpet
                         You need to call the appropriate function when any of these collisions occur between your character and the interactables in the scene
@@ -74,8 +109,16 @@ namespace com
                             - when you collide with the "DoorRotationAxis" call the function "SetDoorOpen" and pass the argument c.gameObject
                         
                     **/
-                    
                 }
+                //void OnCollisionEnter(Collision col)
+                //{
+                //    if (col.gameObject.name == "RedCube")
+                //    {
+                //        Destroy(col.gameObject);
+                //    }
+                        
+                //}
+                
             }
         }
     }
